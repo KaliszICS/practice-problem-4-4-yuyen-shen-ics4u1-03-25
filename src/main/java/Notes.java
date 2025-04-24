02673b81949f1ba7b3ac80ffb46f984e6d61afe7
MrKalisz <72149056+MrKalisz@users.noreply.github.com>
2025-04-24 14:15:04 -0400

    Update Notes.java

--- src/main/java/Notes.java
+++ src/main/java/Notes.java
@@ -1,6 +1,8 @@
 
 class Notes {
 	public static void main(String[] args) {
-
+		//do not need to create an object to use utility classes
+		System.out.println(MyUtilityClass.MAXIMUM);
+		System.out.println(MyUtilityClass.returnZero());
 	}
 }
