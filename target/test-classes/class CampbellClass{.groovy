d5bb8035154f09845a7a7354382259b6299876ec
Yu-Yen, Shen <yu-yen.shen@student.tdsb.on.ca>
2025-04-25 17:09:33 +0000

    yuyen

--- target/test-classes/class CampbellClass{.groovy
+++ target/test-classes/class CampbellClass{.groovy
@@ -1,46 +1,40 @@
-class CampbellClass{
+public final class CampbellClass{
 
-    private String word;
-    private int centi;
-    private double centi0;
-
-    public String convertString(double num0) {
-        this.word = String.valueOf(num0);
-        return this.word;
+    private CampbellClass() {
     }
 
-    public String convertString(int num) {
-        this.word = String.valueOf(num);
-        return this.word;
+    public static String convertString(double num0) {
+       return String.valueOf(num0);
     }
 
-    public String convertString(char ch) {
-        this.word = String.valueOf(ch);
-        return this.word;
+    public static String convertString(int num) {
+        return String.valueOf(num);
     }
 
-    public String convertString(boolean boo) {
-        this.word = String.valueOf(boo);
-        return this.word;
+    public static String convertString(char ch) {
+        return String.valueOf(ch);
+    }
+
+    public static String convertString(boolean boo) {
+        return String.valueOf(boo);
     }
 
     public int convertMetersToCenti(int meter) {
-        this.centi = meter*100;
-        return this.centi;
+        return meter*100;
     }
 
-    public double convertMetersToCenti(double meter0) [
-        this.centi0 = meter0*100;
-        return this.centi0;
-    ]
+    public double convertMetersToCenti(double meter0) {
+        return meter0*100;
+    }
 
     public String removeNonAlpha(String word0, boolean bool){
-        this.word = word0.replaceAll("1234567890!@#$%^&*()_+-=<>", "");
+        String cleaned = word0.replaceAll("[^a-zA-Z]", "");
         if (bool) {
-            return (this.word).toUpperCase();
+            return cleaned.toUpperCase();
         }
         if (!bool){
-            return (this.word).toLowerCase();
+            return cleaned.toLowerCase();
         }
+        return "";
     }
 }
